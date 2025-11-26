edcd42ee28b178bbfb1ec1d15de3b1394f76b9ba
anushree2946 <meanua29@gmail.com>
2025-11-26 22:48:10 +0530

    Fix WSGI settings path

--- backend/project/project/wsgi.py
+++ backend/project/project/wsgi.py
@@ -1,17 +1,6 @@
-"""
-WSGI config for project project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'api.deployment_setting' if 'RENDER_EXTERNAL_HOSTNAME' in os.environ else 'api.settings'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module) 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
 application = get_wsgi_application()
